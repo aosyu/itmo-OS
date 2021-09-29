699d2f60aca0b851d7a21c581919b275c09b42a0
root <root@localhost.localdomain>
2021-09-29 09:41:38 +0300

    abs

--- lab1/6.sh
+++ lab1/6.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 touch full.log
-echo "" > full.log
+#echo "" > full.log
 
 sed -e 's/(\*\*)/Warning:/; s/(II)/Information:/' /var/log/anaconda/X.log >> temp.txt
 
-grep "Warning:" temp.txt >> full.log
+grep "Warning:" temp.txt > full.log
 grep "Information" temp.txt >> full.log
 
 cat full.log

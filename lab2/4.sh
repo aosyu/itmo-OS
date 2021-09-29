b0a22239475f958125e771b5a92c1212033dc224
root <root@localhost.localdomain>
2021-09-29 18:15:32 +0300

    lab2 task 4

--- lab2/4.sh
+++ lab2/4.sh
@@ -1,3 +1,17 @@
 #!/bin/bash
 
-ps -A | sed -n '1p'
+
+for pid in `ps -Ao pid --no-header`
+do
+	file=/proc/$pid
+	if [[ -e $file ]]
+	then
+		PPid=`grep PPid $file/status 2>/dev/null | awk '{print $2}'`
+		sum_exec_runtime=`grep se.sum_exec_runtime $file/sched | awk '{print $3}'`
+		nr_switches=`grep nr_switches $file/sched | awk '{print $3}'`
+		ART=$(echo "$sum_exec_runtime/$nr_switches" | bc -l)
+
+		echo "ProcessID=$pid : Parent_ProcessID=$PPid : Average_Running_Time=$ART"
+	fi
+done | sort -t "=" -n -k 3 > ans4
+

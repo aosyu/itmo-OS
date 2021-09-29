faa81a6a6693d2e3ecc5428e98613b16038ac621
root <root@localhost.localdomain>
2021-09-29 09:13:15 +0300

    task 7 corrected

--- lab1/7.sh
+++ lab1/7.sh
@@ -3,6 +3,6 @@
 touch emails.lst
 echo "" > emails.lst
 
-grep -E -r -i -o -h '[[:alnum:]_-]+@[[:alnum:]]+\.[[:alnum:]]+' --binary-files=text /etc | sort | uniq >> emails.lst
+grep -E -r -h -o --text '[[:alnum:]_-]+@[[:alnum:]]+\.[[:alnum:]]+'   /etc | sort | uniq >> emails.lst
 
 cat emails.lst

53483ddfda9b0208f56146b10ded62d30aee06a3
root <root@localhost.localdomain>
2021-09-29 21:46:49 +0300

    lab2 rask 6

--- lab2/6.sh
+++ lab2/6.sh
@@ -1,2 +1,13 @@
 #!/bin/bash
 
+
+for pid in $(ps -Ao pid --no-header)
+do
+	file=/proc/$pid/status
+	if [[ -f $file ]]
+	then
+		echo `grep VmSize /proc/$pid/status | awk '{print $2}'`
+	fi
+done | sort -n | tail -1
+
+top -o VIRT | head -8 | tail -2

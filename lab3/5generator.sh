cfca81d1c95ecd4138ab3d6258e8b185778b169f
root <root@localhost.localdomain>
2021-09-30 20:08:20 +0300

    abs

--- lab3/5generator.sh
+++ lab3/5generator.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
+number="^[0-9]+$"
 > pipe
 while true; do
 	read LINE
 	echo "$LINE" >> pipe
-	if [[ $LINE == "QUIT" || $LINE ==  ]]; then
+	if [[ $LINE == "QUIT" || !($LINE == '+' || $LINE == '*' || $LINE =~ $number) ]]; then
 		break
 	fi
 done

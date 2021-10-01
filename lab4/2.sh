271625667b8a9df593a462824ce1e9e692cc581b
root <root@localhost.localdomain>
2021-10-01 05:23:36 +0300

    abs

--- lab4/2.sh
+++ lab4/2.sh
@@ -2,8 +2,6 @@
 
 filename=$1
 
-#awk -F "=" '{print $1}' /home/trash.log | awk -v name="$1" -F "/" '($NF == name)'
-
 while read line; do
 	fullname=`echo $line | awk -F '=' '{print $1}'`
 	current=`echo $line | awk -F '=' '{print $1}' | awk -F '/' '{print $NF}'`
@@ -13,13 +11,28 @@ while read line; do
 		read ans <&1
 		if [[ $ans == 'y' ]] ; then
 			path=`echo $fullname | rev | cut -d'/' -f 2- | rev`
+
 			if ! [[ -d $path ]] ; then
 				echo "This catalog is not found. The file will be restored to /home"
-				fullname=/home/$filename
+				path=/home
 			fi
-			ln 
+			fullname=$path/$filename
+
+			while [[ -f $fullname ]] ; do
+				echo "This filename already exists. Wanna rename your file? (y/n):" ;
+				read ans <&1
+				if [[ $ans == 'y' ]] ; then
+					echo "New filename:"
+					read ans <&1
+					fullname=$path/$ans
+				else
+					break
+				fi
+			done
+
+			link=`echo $line | awk -F '=' '{print $2}'`
+			ln /home/.trash/$link $fullname &&
+			rm /home/.trash/$link
 		fi
 	fi
-
-
 done < /home/trash.log

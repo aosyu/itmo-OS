372f9a05dd21a226fc10f869ee12af73a669e381
root <root@localhost.localdomain>
2021-11-29 00:49:44 +0300

    abs

--- lab5/tracking2.sh
+++ lab5/tracking2.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-> 1_current_process_params
-> 2_current_process_params
+> 1_process_params
+> 2_process_params
 > system_memory_params2
 > 5_first_processes2
 
 PID1=`cat pid`
 PID2=`cat pid2`
-while ps -p $PID1 > /dev/null ; do
+while ps -p $PID2 > /dev/null ; do
 	top -p $PID1 | head -8 | tail -1 >> 1_process_params
 	top -p $PID2 | head -8 | tail -1 >> 2_process_params
 	top -p $PID1 | head -5 | tail -2 >> system_memory_params2

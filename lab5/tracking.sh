00a48eb7df9237599ce971fff0a56adcbabb3674
root <root@localhost.localdomain>
2021-11-29 05:10:19 +0300

    abs

--- lab5/tracking.sh
+++ lab5/tracking.sh
@@ -9,10 +9,6 @@ PID=`cat pid`
 while ps -p $PID > /dev/null ; do
 	top -b -n 1 > top.txt
 	cat top.txt | grep $PID >> current_process_params
-	#top -p $PID -d 1 | head -8 | tail -1 >> current_process_params
-	#top -p $PID -d 1 | head -5 | tail -2 >> system_memory_params
-	#top -d 1 | head -12 | tail -5 >> 5_first_processes
-
 	cat top.txt | grep MiB >> system_memory_params
 	cat top.txt | head -12 | tail -5 >> 5_first_processes
 	echo "ho"

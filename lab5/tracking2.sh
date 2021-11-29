cdf21fb76260e709139a6836842e8bf44af80a67
root <root@localhost.localdomain>
2021-11-29 05:24:50 +0300

    abs

--- lab5/tracking2.sh
+++ lab5/tracking2.sh
@@ -8,8 +8,14 @@
 PID1=`cat pid`
 PID2=`cat pid2`
 while ps -p $PID2 > /dev/null ; do
-	top -p $PID1 | head -8 | tail -1 >> 1_process_params
-	top -p $PID2 | head -8 | tail -1 >> 2_process_params
-	top -p $PID1 | head -5 | tail -2 >> system_memory_params2
-	top | head -12 | tail -5 >> 5_first_processes2
+	echo "ku!"
+	top -b -n 1 > top2.txt
+	cat top2.txt | grep $PID1 >> 1_process_params
+	cat top2.txt | grep $PID2 >> 2_process_params
+	cat top2.txt | tac | tail -5 | head -2 >> system_memory_params2
+	cat top2.txt | tac | tail -12 | head -5 >> 5_first_processes
+#	top -p $PID1 | head -8 | tail -1 >> 1_process_params
+#	top -p $PID2 | head -8 | tail -1 >> 2_process_params
+#	top -p $PID1 | head -5 | tail -2 >> system_memory_params2
+#	top | head -12 | tail -5 >> 5_first_processes2
 done
